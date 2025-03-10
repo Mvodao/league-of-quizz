0ae2df4ec3ff39c70688f042f261b827bf1d657c
Calin Ploscaru <ytcognito@gmail.com>
2025-03-10 17:19:36 +0100

    creation du seed

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -2,5 +2,5 @@ class UserGame < ApplicationRecord
   belongs_to :user
   belongs_to :game
   belongs_to :spell
-  belongs_to :category
+  belongs_to :banned_category, class_name: "Category", foreign_key: :"banned_category_id", required: true
 end
