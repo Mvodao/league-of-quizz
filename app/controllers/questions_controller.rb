1773c70b0293c13b9d619bb698277ddb5250abc2
gh api user/emails | jq -r '.[].email <jeremy.thoomas@gmail.com>
2025-03-18 16:45:15 +0100

    merci david

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,20 @@ class QuestionsController < ApplicationController
     @questions = @game.questions
     @current_question_index = params[:id].to_i
     @user_games = @game.user_games
-
     @user_spell = @user_games.find_by(user: current_user).spell
     @is_used = @user_games.find_by(user: current_user).is_used
+    @quickplay = false
 
     if @current_question_index == 1
-      @user_games.find_by(user: current_user).update(score: 0)
-      @user_games.where.not(user: current_user)[0].update(score: 0)
+      @user = false_games.find_by(user: current_user).update(score: 0)
+      #si y a un 2e user faire la ligne en dessous
+      if @game.user_games.count == 2
+        @user_games.where.not(user: current_user)[0].update(score: 0)
+      end
+    end
+
+    if @game.user_games.count == 2
+      @quickplay = true
     end
 
     @question = @questions[@current_question_index-1]
